c85ca05b1626c96c510ec9157685219c40ffad63
Sergey-Nag <nagornyj.seriozha2010@gmail.com>
2021-01-31 04:24:06 +0200

    refactoring, cards isn't comparing

--- js/main.js
+++ js/main.js
@@ -1,109 +1,121 @@
 document.onload = start();
 
-var GAMEDATA = {
-  name: '',
-  layer: 9,
-  images: {
-    9: ['baloon', 'bowl', 'box', 'cap'],
-    16: ['baloon', 'bowl', 'box', 'cap', 'phone', 'planet'],
-    25: ['baloon', 'bowl', 'box', 'cap', 'phone', 'planet', 'sandClock', 'triangle']
-  },
+const GAMEDATA = {
+  name: "",
+  images: ["baloon", "bowl", "box", "cap"],
   layerCards: [],
   cards: [],
-  compareCard: {
-    first: '',
-    second: ''
-  }
-}
+  compareCards: {
+    first: "",
+    second: "",
+  },
+};
 
 function start() {
+  const form = document.getElementById("username");
+  const input = document.getElementById("username-input");
 
-  var form = document.getElementById('name');
-
-  form.onsubmit = function (e) {
-    e.preventDefault();
-    let name = this.children[0].value;
-    GAMEDATA.name = name;
-    loadGame();
-  }
-
+  input.addEventListener("keyup", checkVal);
+  form.addEventListener("submit", setNameAndStart);
 }
 
-function checkVal(inp) {
-  let val = inp.value;
-  let subm = document.querySelector('form input[type="submit"]');
-  if (val.length > 3) {
-    if (/( |`|\~|\.|\*|\&|\?|\!|\'|\"|\)|\()/g.test(val)) {
-      inp.classList.add('error');
-      subm.setAttribute('disabled', true);
-    } else {
-      inp.classList.remove('error');
-      subm.removeAttribute('disabled');
-    }
+function setNameAndStart(e) {
+  e.preventDefault();
+
+  GAMEDATA.name = this.children[0].value;
+
+  loadGame();
+}
+
+function checkVal() {
+  let val = this.value;
+  let subm = document.getElementById("username-submit");
+
+  if (val.length > 3 && /( |`|\~|\.|\*|\&|\?|\!|\'|\"|\)|\()/g.test(val)) {
+    this.classList.add("error");
+    subm.setAttribute("disabled", true);
+  } else {
+    this.classList.remove("error");
+    subm.removeAttribute("disabled");
   }
 }
 
 function loadGame() {
-  let loging = document.querySelector('.d-grid.log');
-  let game = document.getElementById('game');
+  const logLayout = document.querySelector(".d-grid.log");
+  const gameLayout = document.getElementById("game");
+
   setTimeout(() => {
-    loging.style.transition = 'all .8s ease';
-    loging.style.opacity = '0';
+    hideLogMenu(logLayout);
 
     setTimeout(() => {
-      loging.style.display = 'none';
-      game.style.display = 'grid';
-      game.style.transition = 'all .8s ease';
-
+      prepateGameLayout(logLayout, gameLayout);
       startGame();
 
-      setTimeout(() => {
-        game.style.opacity = '1';
-      }, 100);
+      setTimeout(() => showGameLayout(gameLayout), 100);
     }, 700);
   }, 200);
 }
 
+function hideLogMenu(log) {
+  log.style.transition = "all .8s ease";
+  log.style.opacity = "0";
+}
+
+function prepateGameLayout(log, game) {
+  log.style.display = "none";
+  game.style.display = "grid";
+  game.style.transition = "all .8s ease";
+}
+
+function showGameLayout(game) {
+  game.style.opacity = "1";
+}
+
 function setLayer(inp) {
-  let arrInputs = document.querySelectorAll('.card.b');
-  arrInputs.forEach(el => el.classList.remove('active'));
-  let num = inp.getAttribute('data-layer');
+  let arrInputs = document.querySelectorAll(".card.b");
+  arrInputs.forEach((el) => el.classList.remove("active"));
+  let num = inp.getAttribute("data-layer");
   GAMEDATA.layer = num;
-  inp.classList.add('active');
+  inp.classList.add("active");
 }
 
 function startGame() {
-  let wrapper = document.querySelector('.wrapper');
-  let layer = document.getElementById('gameLayer');
-  let layerNum = GAMEDATA.layer;
+  const wrapper = document.querySelector(".wrapper");
+  const layer = document.getElementById("gameLayer");
 
-  wrapper.classList.add('full');
-  let images = GAMEDATA.images[layerNum];
-  layer.classList.add('l-' + layerNum);
+  wrapper.classList.add("full");
+  layer.classList.add("l-9");
 
-  layer.innerHTML = '';
+  layer.innerHTML = "";
 
-  let alreadyBeen = [];
+  prepareCards(GAMEDATA.images);
 
-  for (let i = 0; i < layerNum; i++) {
+  drawCards(layer);
+
+  setTimeout(() => {
+    flipAllCards();
+
+    setTimeout(() => {
+      beginGame("Игра началась");
+    }, 1000);
+  }, 3500);
+}
+
+function prepareCards(images) {
+  const alreadyBeen = [];
+
+  for (let i = 0; i < 9; i++) {
     if (images[i] !== undefined) alreadyBeen.push(images[i]);
     else alreadyBeen.push(images[i - images.length]);
   }
-  alreadyBeen.sort(function (a, b) {
-    return Math.random() - 0.5;
-  });
+
+  randomizeCards(alreadyBeen);
+
   GAMEDATA.layerCards = alreadyBeen;
+}
 
-  drawCards(layer);
-  setTimeout(() => {
-    flipAllCards();
-    setTimeout(() => {
-      beginGame('Игра началась', function () {
-        console.log('ready');
-      });
-    }, 1000);
-  }, 3500);
-
+function randomizeCards(cardsArr) {
+  cardsArr.sort(() => Math.random() - 0.5);
 }
 
 //startGame();
@@ -111,58 +123,77 @@ function startGame() {
 function drawCards(layer) {
   GAMEDATA.layerCards.forEach((el, i) => {
     setTimeout(() => {
-      let elem = document.createElement('div');
-      elem.className = 'wrapp-3D';
-      elem.id = 'wrapp-' + i;
-      elem.setAttribute('onclick', 'flipCard(this)');
-      elem.setAttribute('data-cardname', el);
-
-      if (el !== undefined) elem.innerHTML = '<div class="backface flipped"></div><div class="card flip"><img src="img/' + el + '.png"></div>';
-      else elem.innerHTML = '<div class="backface flipped"></div><div class="card flip  none"></div>';
+      const elem = createCardElement(el, i);
 
       layer.appendChild(elem);
       GAMEDATA.cards.push(elem);
-      setTimeout(() => {
-        elem.style.opacity = '1';
-        elem.style.transform = 'scale(1)';
-      }, i * 70);
 
+      setTimeout(() => {
+        showCard(elem);
+      }, i * 70);
     }, i * 100);
   });
 }
 
-function flipCard(wrapp, isArr) {
-  if (!wrapp.classList.contains('hide')) {
-    let backface = wrapp.children[0];
-    let card = wrapp.children[1];
-    let compare = GAMEDATA.compareCard;
-    if (compare.first == '' || compare.second == '') {
+function showCard(card) {
+  card.style.opacity = "1";
+  card.style.transform = "scale(1)";
+}
 
-      if (card.classList.contains('flipped') && !backface.classList.contains('flipped')) {
-        card.classList.remove('flipped');
-        backface.classList.add('flipped');
-      } else {
-        backface.classList.remove('flipped');
-        card.classList.add('flipped');
-      }
-      if (!isArr) {
-        if (compare.first == '') compare.first = wrapp;
-        else if (compare.second == '') {
-          compare.second = wrapp;
-        }
-        if (compare.first !== '' && compare.second !== '') {
-          if (compare.first.id !== compare.second.id) {
-            if (compare.first.getAttribute('data-cardname') == compare.second.getAttribute('data-cardname')) hideCards(compare);
-            else flipOpenedCards();
-          } else {
-            GAMEDATA.compareCard.first = '';
-            GAMEDATA.compareCard.second = '';
-          }
-        }
-      }
-      console.log(GAMEDATA.compareCard)
-    }
+function createCardElement(name, i) {
+  const elem = document.createElement("div");
+  elem.className = "wrapp-3D";
+  elem.id = "wrapp-" + i;
+  elem.setAttribute("data-cardname", name);
+
+  elem.innerHTML =
+    name !== undefined
+      ? `<div class="backface flipped"></div><div class="card flip"><img src="img/${name}.png"></div>`
+      : '<div class="backface flipped"></div><div class="card flip none"></div>';
+
+  return elem;
+}
+
+function flipCard(wrapp) {
+  if (wrapp.classList.contains("hide")) return;
+
+  const backface = wrapp.children[0];
+  const card = wrapp.children[1];
+  const compare = GAMEDATA.compareCards;
+
+  if (compare.first !== "" && compare.second !== "") return;
+
+  if (card.classList.contains("flipped")) {
+    card.classList.remove("flipped");
+    backface.classList.add("flipped");
+  } else {
+    backface.classList.remove("flipped");
+    card.classList.add("flipped");
   }
+  console.log(GAMEDATA.compareCards);
+}
+
+function compareCards(name, id) {
+  const compare = GAMEDATA.compareCards;
+  
+  if (compare.first !== "" && compare.second !== "") return;
+
+  if (compare.first.name === "" && compare.second.name === "") {
+    compare.first.name = name;
+  } else if (compare.second.name === name) {
+    compare.second.name = name;
+  }
+
+  if (compare.first.id === compare.second.id) {
+    GAMEDATA.compareCards.first = "";
+    GAMEDATA.compareCards.second = "";
+
+    return;
+  }
+
+  if (compare.first.name === compare.second.name) hideCards(compare);
+  else flipOpenedCards();
+
 }
 
 function returnRandom(max) {
@@ -171,41 +202,42 @@ function returnRandom(max) {
 
 function flipOpenedCards() {
   setTimeout(() => {
-
     GAMEDATA.cards.forEach((el) => {
-      if (el.children[0].classList.contains('flipped')) {
-        el.children[0].classList.remove('flipped');
-        el.children[1].classList.add('flipped');
+      if (el.children[0].classList.contains("flipped")) {
+        el.children[0].classList.remove("flipped");
+        el.children[1].classList.add("flipped");
       }
     });
-    GAMEDATA.compareCard.first = '';
-    GAMEDATA.compareCard.second = '';
+    GAMEDATA.compareCards.first = "";
+    GAMEDATA.compareCards.second = "";
   }, 1000);
 }
 
-function hideCards(name) {
+function hideCards() {
   setTimeout(() => {
-    let cards = GAMEDATA.compareCard;
-    cards.second.classList.add('hide');
+    const cards = document.querySelectorAll('.wrapp-3D > .backface.flipped');
+
     setTimeout(() => {
-      cards.first.classList.add('hide');
-      GAMEDATA.compareCard.first = '';
-      GAMEDATA.compareCard.second = '';
+      cards.forEach(card => card.classList.add("hide"));
+
+      GAMEDATA.compareCards.first = "";
+      GAMEDATA.compareCards.second = "";
     }, 200);
   }, 1000);
 }
 
 function flipAllCards() {
-  GAMEDATA.cards.forEach(el => flipCard(el, true));
-  return true;
+  GAMEDATA.cards.forEach((el) => {
+    flipCard(el);
+  });
 }
 
-function beginGame(text, callback) {
-  let msg = document.getElementById('message');
-  let popup = document.querySelector('.popup');
+function beginGame(text) {
+  const msg = document.getElementById("message");
+  const popup = document.querySelector(".popup");
   msg.innerText = text;
 
-  popup.style.display = 'block';
+  popup.style.display = "block";
   setTimeout(() => {
     popup.style.opacity = 1;
     setTimeout(() => {
@@ -215,11 +247,22 @@ function beginGame(text, callback) {
         setTimeout(() => {
           popup.style.opacity = 0;
           setTimeout(() => {
-            popup.style.display = 'none';
-            callback();
+            popup.style.display = "none";
+
+            activeFlipOnCards();
           }, 200);
         }, 600);
       }, 2000);
     }, 200);
   }, 100);
+}
+
+function activeFlipOnCards() {
+  const layer = document.getElementById("gameLayer");
+
+  layer.addEventListener("click", (e) => {
+    const card = e.target.closest(".wrapp-3D");
+    flipCard(card);
+    compareCards(card.dataset.cardname, card.id);
+  });
 }
