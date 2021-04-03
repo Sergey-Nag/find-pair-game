6b7200449e19b0e45540a7597e22565c0cac5cd4
Sergey-Nag <nagornyj.seriozha2010@gmail.com>
2021-04-03 20:52:09 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
-function App({ abc }) {
-  return (
-    <div className={abc} />
-  );
+function App() {
+  return <div className="App" />;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App abc="hello" />
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
