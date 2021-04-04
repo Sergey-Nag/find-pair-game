f4497d89e46fa1927bf2d2fdf592cf58d12dca96
Sergey-Nag <nagornyj.seriozha2010@gmail.com>
2021-04-04 21:59:16 +0300

    player reducer

---
+++ src/store/player/playerTypes.js
@@ -0,0 +1,3 @@
+export const SET_NICK_PLAYER = 'player/setNickname';
+export const SET_SCORE_PLAYER = 'player/setScore';
+export const CLEAR_SCORE_PLAYER = 'player/clearScore';
--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 import leaderboardReducer from './leaderboard/leaderboardReducer';
 import gamemodeReducer from './gamemode/gamemodeReducer';
 import cardsReducer from './cards/cardsReducer';
+import playerReducer from './player/playerReducer';
 
 const rootReducer = combineReducers({
+  player: playerReducer,
   gamemode: gamemodeReducer,
   leaderboard: leaderboardReducer,
   cards: cardsReducer,
