02f59e3056892b26f40cb6b5c13a0529f66da3ae
Sergey-Nag <nagornyj.seriozha2010@gmail.com>
2021-04-05 03:13:42 +0300

    start animating flipping carts

---
+++ src/components/GameCard/GameCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './GameCard.scss';
+
+function GameCard() {
+  return (
+    <div className="card">
+      <div className="card__shirt" />
+      <div className="card__shadow" />
+    </div>
+  );
+}
+
+export default GameCard;
