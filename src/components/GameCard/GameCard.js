6db3e570a9ad3372fa316600d7f512d56048766b
Sergey-Nag <nagornyj.seriozha2010@gmail.com>
2021-04-06 01:39:01 +0300

    fipping cards

--- src/components/GameCard/GameCard.js
+++ src/components/GameCard/GameCard.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './GameCard.scss';
+import gameCardImages from '../../utils/gameCardImages';
 
-function GameCard({ a }) {
+function GameCard({ id, isFlipped, image }) {
   return (
-    <div className={`card${a ? ' card_flipped' : ''}`}>
+    <div id={`card-${id}`} className={`card${isFlipped ? ' card_flipped' : ''}`}>
       <div className="card__shirt" />
+      <div className="card__face" style={{ backgroundImage: `url(${gameCardImages[image]})` }} />
       <div className="card__shadow" />
     </div>
   );
---
+++ src/utils/gameCardImages.js
@@ -0,0 +1,27 @@
+import androidImage from '../assets/img/card-images/android.svg';
+import acerImage from '../assets/img/card-images/acer.svg';
+import appleImage from '../assets/img/card-images/apple.svg';
+import beatsImage from '../assets/img/card-images/beats.svg';
+import hpImage from '../assets/img/card-images/hp.svg';
+import lenovoImage from '../assets/img/card-images/lenovo.svg';
+import nokiaImage from '../assets/img/card-images/nokia.svg';
+import intelImage from '../assets/img/card-images/intel.svg';
+import dellImage from '../assets/img/card-images/dell.svg';
+import ciscoImage from '../assets/img/card-images/cisco.svg';
+import htcImage from '../assets/img/card-images/htc.svg';
+
+const gameCardImages = {
+  android: androidImage,
+  acer: acerImage,
+  apple: appleImage,
+  beats: beatsImage,
+  hp: hpImage,
+  lenovo: lenovoImage,
+  nokia: nokiaImage,
+  intel: intelImage,
+  dell: dellImage,
+  cisco: ciscoImage,
+  htc: htcImage,
+};
+
+export default gameCardImages;
