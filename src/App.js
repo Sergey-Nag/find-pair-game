0beb854f516ffdd9980d3f88cbc4021606548a6b
ihor1998 <ihor.milshyn@gmail.com>
2021-04-05 22:18:53 +0300

    button-component__v1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import Button from './components/Button';
+
+function click(e) {
+  // const { target } = e;
+  // if (target.closest('.button__box')) {
+  //   const checkbox = document.querySelector('.button__checkbox');
+  //   checkbox.classList.toggle('active');
+  // }
+}
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      {' '}
+      <Button classes="button button__primary" clickHandler={click}>
+        Play
+      </Button>
+      <Button classes="button button__secondary" clickHandler={click}>
+        Play
+      </Button>
+      <Button classes="button button__danger" clickHandler={click}>
+        Play
+      </Button>
+      <Button classes="button button__large" clickHandler={click}>
+        Play
+      </Button>
+      <Button isCheckbox checked classes="button button__primary" clickHandler={click}>
+        4X4
+      </Button>
+    </div>
+  );
 }
 
 export default App;
