22d629f0a6a26f92075d2b6c76624a2327339732
Sergey-Nag <nagornyj.seriozha2010@gmail.com>
2021-04-06 15:41:38 +0300

    card components

--- src/components/GameCard/GameCard.js
+++ src/components/GameCard/GameCard.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './GameCard.scss';
 import gameCardImages from '../../utils/gameCardImages';
 
-function GameCard({ id, isFlipped, image }) {
+function GameCard({ card, id }) {
+  const {
+    isFlipped, name,
+  } = card;
+
   return (
     <div id={`card-${id}`} className={`card${isFlipped ? ' card_flipped' : ''}`}>
       <div className="card__shirt" />
-      <div className="card__face" style={{ backgroundImage: `url(${gameCardImages[image]})` }} />
+      <div className="card__face" style={{ backgroundImage: `url(${gameCardImages[name]})` }} />
       <div className="card__shadow" />
     </div>
   );
