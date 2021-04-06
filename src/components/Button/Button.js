98b7a5abeaf9419522fbf4fb918e981569b46bb7
Sergey-Nag <nagornyj.seriozha2010@gmail.com>
2021-04-06 18:31:17 +0300

    change button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,8 +4,13 @@ import './Button.scss';
 function Button({
   children, clickHandler, classes, isCheckbox, checked,
 }) {
+  const variantClass = classes
+    .split(' ')
+    .map((el) => `button__${el}`)
+    .join('');
+
   return (
-    <button className={`button ${classes.split(' ').map((el) => `button__${el}`).join('')}`} type="button" onClick={clickHandler}>
+    <button className={`button ${variantClass}`} type="button" onClick={clickHandler}>
       {isCheckbox && (
         <div className="button__box">
           <div className={`button__checkbox ${checked ? 'active' : ''}`} />
