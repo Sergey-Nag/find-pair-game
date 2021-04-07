109cbd7067a2a8f88d4d2e7870741c99729afcd8
Sergey-Nag <nagornyj.seriozha2010@gmail.com>
2021-04-07 03:44:51 +0300

    info display actual data from store

--- src/pages/Gamepage/Gamepage.js
+++ src/pages/Gamepage/Gamepage.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Button from '../../components/Button';
 import Leaderboard from '../../components/Leaderboard';
 import './Gamepage.scss';
 
 function Gamepage() {
+  const player = useSelector((state) => state.player);
+  const game = useSelector((state) => state.game);
+
+  const nickname = player.nickname ?? '...';
+  const time = game.time ?? '--:--';
+
   return (
     <div className="game">
       <div className="game__interface">
         <div className="game__info">
-          <span className="game__nickname">Username</span>
-          <span className="game__time">01:00</span>
+          <span className="game__nickname">{nickname}</span>
+          <span className="game__time">{time}</span>
         </div>
         <div className="game__leaderboard">
           <Leaderboard leaders={[]} />
@@ -18,8 +25,8 @@ function Gamepage() {
           <Button variant="danger">Surrender</Button>
         </div>
       </div>
-      <div className="game__playground"></div>
-      <div className="game__loader"></div>
+      <div className="game__playground" />
+      <div className="game__loader" />
     </div>
   );
 }
