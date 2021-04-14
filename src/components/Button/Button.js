65c3c2c06b87d1fc482d94a6b56ae04e3034ad24
ihor1998 <ihor.milshyn@gmail.com>
2021-04-14 20:25:54 +0300

    pause-modal

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,10 +2,14 @@ import React from 'react';
 import './Button.scss';
 
 function Button({
-  children, clickHandler, variant, isCheckbox, checked,
+  children, clickHandler, variant, isCheckbox, checked, type,
 }) {
   return (
-    <button className={`button button__${variant}`} type="button" onClick={clickHandler}>
+    <button
+      className={`button button__${variant}`}
+      type={type ? 'submit' : 'button'}
+      onClick={clickHandler}
+    >
       {isCheckbox && (
         <div className="button__box">
           <div className={`button__checkbox ${checked ? 'active' : ''}`} />
